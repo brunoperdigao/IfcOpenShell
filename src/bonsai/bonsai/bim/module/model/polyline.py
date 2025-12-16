238b82c988bb19443dc04e7a907412ae22dd4395
Bruno Perdigão <bruno@brunopo.com>
2025-12-16 16:13:19 -0300

    Snap: improve performance of wireframe objects intersection. Enhances the performance of mouse intersection checks for wireframe objects. Details: - Calculated the intersection with the mouse in 2D pixels first. - Converted objects to a BVH Tree to reduce the number of edges checked against the mouse position.

--- src/bonsai/bonsai/bim/module/model/polyline.py
+++ src/bonsai/bonsai/bim/module/model/polyline.py
@@ -987,7 +987,7 @@ class PolylineOperator:
                     tool.Polyline.calculate_x_y_and_z(context, self.input_ui, self.tool_state)
 
                 tool.Blender.update_viewport()
-                return {"RUNNING_MODAL"}
+            return {"RUNNING_MODAL"}
 
     def get_product_preview_data(self, context: bpy.types.Context, relating_type: ifcopenshell.entity_instance) -> None:
         if tool.Model.get_usage_type(relating_type) == "PROFILE":
@@ -1058,6 +1058,7 @@ class PolylineOperator:
         self.tool_state.axis_method = None
         self.tool_state.plane_method = None
         self.tool_state.mode = "Mouse"
+        tool.Raycast.clear_snap_objs()
         self.visible_objs = tool.Raycast.get_visible_objects(context)
         for obj in self.visible_objs:
             if bbox_2d := tool.Raycast.get_on_screen_2d_bounding_boxes(context, obj):
